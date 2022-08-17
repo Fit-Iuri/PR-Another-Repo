38540b39eed9b773bafda31f0398bd1cf22bbb71
Fit-Iuri <iuri.mendes@fitbank.com.br>
2022-08-17 10:43:38 -0300

    ok

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -l
+#!/bin/sh
 
 set -e
 set -x
@@ -33,7 +33,7 @@ echo "Cloning destination git repository"
 git clone "https://$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 
 echo "Copying contents to git repo"
-mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER/
+# mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER/
 cp $INPUT_SOURCE_FOLDER "$CLONE_DIR/$INPUT_DESTINATION_FOLDER/"
 cd "$CLONE_DIR"
 git checkout -b "$INPUT_DESTINATION_HEAD_BRANCH"
